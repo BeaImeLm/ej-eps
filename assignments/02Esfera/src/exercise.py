b87c49ab63ea598ac5282a8a613bd50260a12db1
BeaImeLm <beatriz.lm@culiacan.tecnm.mx>
2021-08-18 05:13:54 +0000

    Código Reescrito de 02Esfera

--- assignments/02Esfera/src/exercise.py
+++ assignments/02Esfera/src/exercise.py
@@ -10,6 +10,12 @@ def main():
 
     radio = float(input("Ingrese el valor del radio de la esfera: "))
 
+    volumen = 4 / 3 * math.pi * math.pow(radio,3)
+    area = 4 * math.pi * math.pow(radio,2)
+
+    print(f"El volumen de la esfera con radio {radio} es: {volumen} unidades cúbicas")
+    print(f"El área de la esfera con radio {radio} es: {area} unidades cuadradas")
+
 
 
 if __name__=='__main__':
